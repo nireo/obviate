f1a5447fb1982036ecb49edb1108d58cded36ddb
nireo <eemil.lehtikangas1@gmail.com>
2020-12-19 23:13:38 +0200

    added some basic request types

--- requests.go
+++ requests.go
@@ -1,6 +1,9 @@
 package obviate
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func Get(url string) (*http.Response, error) {
 	response, err := http.Get("http://www.example.com")
@@ -9,4 +12,40 @@ func Get(url string) (*http.Response, error) {
 	}
 
 	return response, nil
+}
+
+func Delete(url string) (*http.Response, error) {
+	request, err := http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return request.Response, nil
+}
+
+func Patch(url string, body io.Reader) (*http.Response, error) {
+	request, err := http.NewRequest(http.MethodPatch, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return request.Response, nil
+}
+
+func Post(url string, body io.Reader, contentType string) (*http.Response, error) {
+	response, err := http.Post(url, contentType, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func Put(url string, body io.Reader) (*http.Response, error) {
+	request, err := http.NewRequest(http.MethodPut, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return request.Response, nil
 }
